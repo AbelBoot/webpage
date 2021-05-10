4e116e062030acab32af22c17d2d6859ec1de024
Abel <abellocati@homail.com>
2021-05-10 16:53:10 +0100

    fixed image on mobile

--- src/theme/PChemStyle.js
+++ src/theme/PChemStyle.js
@@ -67,7 +67,8 @@ flex-wrap: wrap;
 `
 export const PCDIVVERT_1_Sm = styled.div`  
 flex-basis: 100%;
-height: ${props => props.smallHeight}vh;
+min-height: 50vh;
+//height: ${props => props.smallHeight}vh;
 padding-right: ${props => props.paddingRight}vw;
 padding-left: ${props => props.paddingLeft}vw;
 
