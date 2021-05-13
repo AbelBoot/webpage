7819d6a8d1be58ee23a395c9ebf76afe0d742375
Abel <abellocati@homail.com>
2021-05-13 16:47:34 +0100

    fixed image on mobile

--- src/components/PChem1.js
+++ src/components/PChem1.js
@@ -41,7 +41,7 @@ export const PChem1 = () => {
                   <Image img={"dibenzoTS"} 
 					bottom="2" 
 					right="5"
-					height="40"
+					minHeight="40"
 					/>
 		          </PCDIVVERT_2>
                 </>
