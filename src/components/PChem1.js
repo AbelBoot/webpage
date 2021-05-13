da2649ac1fbdeab4bb9cac69988b2dd4ed2fd799
Abel <abellocati@homail.com>
2021-05-13 14:58:01 +0100

    fixed image on mobile

--- src/components/PChem1.js
+++ src/components/PChem1.js
@@ -14,7 +14,7 @@ export const PChem1 = () => {
 		    { widthS < 600 
 		    	? <>
 				<PCDIVVERT_0_Sm>
-      			  <PCDIVVERT_1_Sm paddingRight="3" paddingLeft="1" smallHeight="20">
+      			  <PCDIVVERT_1_Sm paddingRight="3" paddingLeft="1" smallHeight="80">
                     <Image img={"dibenzoTS"} 
 					bottom="2" 
 					right="5"
