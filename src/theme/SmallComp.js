245ab20c9ffad765c86faff43e8798744351ee85
Abel <abellocati@homail.com>
2021-05-13 16:31:53 +0100

    fixed image on mobile

--- src/theme/SmallComp.js
+++ src/theme/SmallComp.js
@@ -313,7 +313,8 @@ export const Image = styled.img.attrs(props => ({
   right: ${props => props.right}vw;
   max-width: ${props => props.maxWidth}%;
   min-width: ${props => props.minWidth}%;
-  min-height: ${props => props.minHeight}vh; 
+  //height: ${props => props.height}vw; 
+  height: ${props => props.minHeight}vh; 
   background-size: contain;
   background-repeat: no-repeat;
   ${(props) => props.justify && css `
