6e0dd5e2582e204d5b576c41fdce12e75ecdf776
Abel <abellocati@homail.com>
2021-06-02 17:56:16 +0100

    Updates

---
+++ precache-manifest.40b1d4b1a7e657ca21e2d8d85325a879.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9bf5f5e9ab12e7398b489a78462261c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "456c58864e12446e5216",
+    "url": "/portfolio/static/css/main.626ac9ae.chunk.css"
+  },
+  {
+    "revision": "d9d710c59fed9c011bb0",
+    "url": "/portfolio/static/js/2.cef8457d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.cef8457d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "456c58864e12446e5216",
+    "url": "/portfolio/static/js/main.e6f5e081.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
--- src/theme/variables.js
+++
@@ -1,9 +0,0 @@
-export const violet = "#7D203F"
-export const pink = "#F2D1DE"
-export const lightPink = "#FAFAFA"
-export const grey = "#758C98"
-export const lightGrey = "#879FC1"
-export const yellow = "#FAA916"
-export const white = "#FFFFFF"
-
-
